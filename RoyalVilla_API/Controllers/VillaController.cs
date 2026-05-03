f39c107e99e659c255f9c8861615841a14defaff
002586 <Vinh140698>
2026-05-03 15:22:57 +0700

    Add project files.

---
+++ RoyalVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoyalVilla_API.Controllers
+{
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/villas")]
+        public string GetVillas()
+        {
+            return "Get all Villas";
+        }
+    }
+}
